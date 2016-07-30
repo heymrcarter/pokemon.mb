9ea05eb37a1a5ebed6c6e8a60977467410dca83c
= <heymrcarter14@gmail.com>
2016-07-29 20:08:17 -0400

    Add project files.

---
+++ Pokemon.Mb/Modules/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace Pokemon.Mb.Modules
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
